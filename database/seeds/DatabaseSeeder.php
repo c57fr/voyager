e81c8fbdd1b3e25ca9d242be5aeb96db94008ef0
GrCOTE7 <GrCOTE7@Gmail.com>
2017-05-09 00:55:21 +0200

    Pour que db:seed trouve les seeders ds le dossier

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,15 +2,35 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
+class DatabaseSeeder extends Seeder {
+
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run() {
+
+    // $this->call(UsersTableSeeder::class);
+    if (App::environment() === 'production') exit();
+
+//    Eloquent::unguard();
+
+    // Truncate all tables, except migrations
+    //    $tables = DB::select('SHOW TABLES');
+    //    foreach ($tables as $table) {
+    //      if ($table->Tables_in_database !== 'migrations') {
+    //        DB::table($table->Tables_in_database)->truncate();
+    //      }
+    //    }
+
+    // Find and run all seeders
+    $classes = require base_path() . '/vendor/composer/autoload_classmap.php';
+    foreach ($classes as $class) {
+      if (strpos($class, 'TableSeeder') !== false) {
+        $seederClass = substr(last(explode('/', $class)), 0, -4);
+        $this->call($seederClass);
+      }
     }
+  }
 }
