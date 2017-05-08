f065b7510a042674ed315c3a510d1cc4a6065b9b
GrCOTE7 <GrCOTE7@Gmail.com>
2017-05-09 00:14:04 +0200

    Init Voyager

--- config/app.php
+++ config/app.php
@@ -163,10 +163,13 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+
         /*
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        TCG\Voyager\VoyagerServiceProvider::class,
+
 
         /*
          * Application Service Providers...
